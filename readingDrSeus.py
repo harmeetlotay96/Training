99b7a2e5b8ae298a6849eadd73e3b18df203a35b
Harmeet Singh <lotteyharmeet@gmail.com>
2024-05-03 11:18:26 -0700

    Modified code

--- readingDrSeus.py
+++ readingDrSeus.py
@@ -1,3 +1,4 @@
+# Added this comment to show changes in git hub
 with open('DrSeus.txt', 'r', encoding='latin-1') as my_file: # closes the file 
     my_poem = my_file.read()
     print(my_poem)
@@ -5,7 +6,7 @@ with open('DrSeus.txt', 'r', encoding='latin-1') as my_file: # closes the file
 
 with open('DrSeus.txt', 'r', encoding='latin-1') as my_file2:
     for line in my_file2:
-        #print(line.rstrip('\n'))
+        print(line.rstrip('\n')) 
         my_new_line = line.lower().replace('you ', 'me ')
         if 'you' not in line.lower():
             print(line, end="") # both works

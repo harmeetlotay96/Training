2a47c1393d82a036baa1175b733866901234e4fe
Harmeet Singh <lotteyharmeet@gmail.com>
2024-05-03 11:32:04 -0700

    Testing branches

--- readingDrSeus.py
+++ readingDrSeus.py
@@ -6,7 +6,7 @@ with open('DrSeus.txt', 'r', encoding='latin-1') as my_file: # closes the file
 
 with open('DrSeus.txt', 'r', encoding='latin-1') as my_file2:
     for line in my_file2:
-        print(line.rstrip('\n')) 
+        #print(line.rstrip('\n')) 
         my_new_line = line.lower().replace('you ', 'me ')
         if 'you' not in line.lower():
             print(line, end="") # both works
